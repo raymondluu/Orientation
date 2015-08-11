c3f6cb6eb42cd658db546073376825faf701ec4e
raymondluu <raymond.luu1@gmail.com>
2015-08-11 11:44:38 -0700

    Adding files
    Adding Orientation files

---
+++ src/Hello.java
@@ -0,0 +1,31 @@
+/*
+ * Raymond Luu
+ * 
+ * TCSS 305 - Winter 2012
+ * Assignment 0 - Orientation
+ */
+/**
+ * This program displays a message.
+ * 
+ * @author Raymond
+ * @version 4 January 2012
+ *
+ */
+public final class Hello {
+  /**
+   * Private Constructor.
+   */
+  private Hello() {
+    //do nothing
+  }
+  /**
+   * Line of command that prints out a message.
+   * 
+   * @param the_args This initial parameter is final.
+   */
+  public static void main(final String[] the_args) {
+    System.out.println("Hello World");
+    
+  }
+  
+}
